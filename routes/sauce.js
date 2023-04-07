30f7580d70c8fd05c2ad4f7391a28d35d68b4a88
Martin Evrard <martin.evrard67@gmail.com>
2023-04-08 00:15:09 +0200

    Authentification & multer middlewares configuration

--- routes/sauce.js
+++ routes/sauce.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const sauceCtrl = require("../controllers/sauce");
-
 const router = express.Router();
 
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+// TODO -- Penser à intégrer multer aux routes POST et PUT
 router.get("/", sauceCtrl.getAllSauces)
+
 
 module.exports = router;
