560346452363c20c9de4cc249860a7ca0c7d6890
Martin Evrard <martin.evrard67@gmail.com>
2023-04-12 22:44:04 +0200

    comments removed

--- models/user.js
+++ models/user.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
-// ! Le package mongoose-unique-validator n'est utilisable qu'avec une version mongoose ANTERIEURE à 7.0.0
-
 const userSchema = mongoose.Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true}
